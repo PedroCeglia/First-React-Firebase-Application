2f0e70fcda81a1e53b3b2afd8eb57973d39908f8
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-10-21 22:57:14 -0300

    UI Config Perfil

--- src/Widget/NavMain/index.js
+++ src/Widget/NavMain/index.js
@@ -15,15 +15,34 @@ export default function NavMain(){
             <div className='other active'>
                 <div className='perfil active'>
                     <div className='cabecalho-menu-icon'>
-                        <img src='assets/menu.png' alt='Back Icon' title='Voltar'/>
+                        <img src='assets/back.png' alt='Back Icon' title='Voltar'/>
                         <span>Perfil</span>
                     </div>
                     <div className='body-perfil-menu-icon'>
                         <div className='foto-perfil'>
-                            <div className='content'></div>
+                            <label for='input-foto-perfil' className='content'>
+                                <img src='assets/camera.png' alt='Camera Icon'/>
+                                <span>Troque de foto</span>
+                            </label>
+                            <input className='input-file' type='file' id='input-foto-perfil'/>
                             <img src='assets/perfil.png' alt='Perfil Photo'/>
                         </div>
-
+                        <div className='inputs-area'>
+                            <div className='input-name'>
+                                <label for='input-nome' >Nome :</label>
+                                <div>
+                                    <input name='input-nome' id='input-nome' type='text'></input>
+                                    <img src='assets/pencil.png' alt='Draw Icon'/>                                
+                                </div>
+                            </div>
+                            <div className='input-description'>
+                                <label for='input-description' >Descrição :</label>
+                                <div>
+                                    <input name='input-description' id='input-description' type='text'></input>
+                                    <img src='assets/pencil.png' alt='Draw Icon'/>                                
+                                </div>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
