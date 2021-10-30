0bd89ec62f92444a8cc85297948d7430de9918fc
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-10-30 17:55:26 -0300

    create open Image

--- src/Widget/ContentMain/ContentContent/MensageList/Mensage/index.js
+++ src/Widget/ContentMain/ContentContent/MensageList/Mensage/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.css'
 
+// Import Widgets
+import OpenMidia from "./OpenMidia";
+
 export default function Mensage(props){
+
+    const [active, setActive] = useState('open-midia-container')
+
+    function handlerImage(){
+        setActive("open-midia-container active")
+    }
+
     if(props.foto != null){
         if(props.mensage != null && props.mensage != "image.jpeg"){
             return(
                 <div className={props.smsClass}>
-                    <img src={props.foto}/>
+                    <img src={props.foto} alt='image Mensage' onClick={handlerImage}/>
+                    <OpenMidia 
+                        foto={props.foto}
+                        active={active}
+                        setActiveProps={setActive}
+                    />
                     <p>                
                         {props.mensage}
                         <span className='sms-hour'>{props.smsHour}</span>
@@ -16,7 +31,12 @@ export default function Mensage(props){
         } else{
             return(
                 <div className={props.smsClass}>
-                    <img src={props.foto}/>
+                    <img src={props.foto} alt='image Mensage' onClick={handlerImage}/>
+                    <OpenMidia 
+                        foto={props.foto}
+                        active={active}
+                        setActiveProps={setActive}
+                    />
                     <p>                
                         <span className='sms-hour'>{props.smsHour}</span>
                     </p>
